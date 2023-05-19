7863a5ad0e4a80627c7887c8387a5391ecef2950
Dan Willert <dwillert4@gmail.com>
2023-05-19 17:45:30 -0400

    typo

--- src/js/golf.js
+++ src/js/golf.js
@@ -167,8 +167,8 @@ let populateCard = () => {
 async function retrieveGolfData(){
     console.log("Running API")
     const sts = new AWS.STS();
-    var params = {RoleArn: "arn:aws:iam::518463288977:role/aws-elasticbeanstalk-ec2-role"}
-    sts.assumeRole(params, function (err, data) {
+    var arn = {RoleArn: "arn:aws:iam::518463288977:role/aws-elasticbeanstalk-ec2-role"}
+    sts.assumeRole(arn, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else     console.log(data);           // successful response
       });
