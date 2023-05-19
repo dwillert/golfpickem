6e4e753a7b603cfac54e2dc143e4bda02c0e1444
Dan Willert <dwillert4@gmail.com>
2023-05-19 17:46:54 -0400

    update sts call

--- src/js/golf.js
+++ src/js/golf.js
@@ -167,7 +167,7 @@ let populateCard = () => {
 async function retrieveGolfData(){
     console.log("Running API")
     const sts = new AWS.STS();
-    var arn = {RoleArn: "arn:aws:iam::518463288977:role/aws-elasticbeanstalk-ec2-role"}
+    var arn = {RoleArn: "arn:aws:iam::518463288977:role/aws-elasticbeanstalk-ec2-role", RoleSessionName: "s3AssumeRoleSession",}
     sts.assumeRole(arn, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else     console.log(data);           // successful response
