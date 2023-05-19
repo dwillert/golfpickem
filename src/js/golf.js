217182feb77c223e7a120f0daacea6fbc1e20318
Dan Willert <dwillert4@gmail.com>
2023-05-19 17:51:48 -0400

    log

--- src/js/golf.js
+++ src/js/golf.js
@@ -168,10 +168,12 @@ async function retrieveGolfData(){
     console.log("Running API")
     const sts = new AWS.STS();
     var arn = {RoleArn: "arn:aws:iam::518463288977:role/aws-elasticbeanstalk-ec2-role", RoleSessionName: "s3AssumeRoleSession",}
-    sts.assumeRole(arn, function (err, data) {
+    var creds = sts.assumeRole(arn, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else     console.log(data);           // successful response
-      });
+    });
+    console.log("Done")
+    console.log(creds)
 
 
     const s3Bucket = new AWS.S3({ 
