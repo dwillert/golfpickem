acd0f0a4afd4023933741b1df0d28b0deb092278
Dan Willert <dwillert4@gmail.com>
2023-05-19 12:29:37 -0400

    update route

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 3000
 app.listen(port, () => {
     console.log(`app running on port ${port}`)
 });
+app.use("/", express.static("src"));
 
-app.use(express.static("src"))
+app.get('/', (req, res)=>{
+    res.sendStatus(200);
+})
 
 
