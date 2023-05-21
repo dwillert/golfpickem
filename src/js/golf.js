18460ed66de9b40ca33913fd587b0d9dbb7e5461
Dan Willert <dwillert4@gmail.com>
2023-05-21 16:22:53 -0400

    debug

--- src/js/golf.js
+++ src/js/golf.js
@@ -208,7 +208,7 @@ async function retrieveGolfData(){
     //     else     console.log(data);
     // });  
     // console.log(answer)
-    const data = await getUrl(s3Bucket, params);
+    let data = await getUrl(s3Bucket, params);
     console.log("DATA ", data)
 
     const params2 = {
